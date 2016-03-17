966289407df81bee462b28bf6bbbc2c67ca46076
Noah Weninger <nweninge@ualberta.ca>
2016-03-16 19:37:19 -0600

    broken reverse decimate

--- test/main.cpp
+++ test/main.cpp
@@ -7,10 +7,22 @@
 #include <iostream>
 #include <set>
 
+using namespace std;
+using namespace Eigen;
+using namespace igl;
+
+struct MeshModification {
+    RowVectorXd v1;
+    RowVectorXd v2;
+    RowVectorXd v3;
+    int fi;
+    RowVectorXi f;
+    MeshModification(RowVectorXd v1, RowVectorXd v2, RowVectorXd v3, int fi,
+                     RowVectorXi f)
+        : v1(v1), v2(v2), v3(v3), fi(fi), f(f) {}
+};
+
 int main(int argc, char *argv[]) {
-    using namespace std;
-    using namespace Eigen;
-    using namespace igl;
     cout << "Usage: ./703_Decimation_bin [filename.(off|obj|ply)]" << endl;
     cout << "  [space]  toggle animation." << endl;
     cout << "  'r'  reset." << endl;
@@ -34,6 +46,7 @@ int main(int argc, char *argv[]) {
     // If an edge were collapsed, we'd collapse it to these points:
     MatrixXd C;
     int num_collapsed;
+    std::vector<MeshModification> mods;
 
     // Function for computing cost of collapsing edge (lenght) and placement
     // (midpoint)
@@ -48,6 +61,7 @@ int main(int argc, char *argv[]) {
 
     // Function to reset original mesh and data structures
     const auto &reset = [&]() {
+        mods.clear();
         F = OF;
         V = OV;
         edge_flaps(F, E, EMAP, EF, EI);
@@ -68,12 +82,14 @@ int main(int argc, char *argv[]) {
         viewer.data.set_face_based(true);
     };
 
-    const auto &pre_draw = [&](igl::viewer::Viewer &viewer) -> bool {
+    const auto &collapse_edges = [&](igl::viewer::Viewer &viewer) -> bool {
         // If animating then collapse 10% of edges
         if (viewer.core.is_animating && !Q.empty()) {
             bool something_collapsed = false;
             // collapse edge
-            const int max_iter = std::ceil(0.1 * Q.size());
+            const int max_iter = 1;
+            MatrixXd OOV = V;
+            MatrixXi OOF = F;
             for (int j = 0; j < max_iter; j++) {
                 if (!collapse_edge(shortest_edge_and_midpoint, V, F, E, EMAP,
                                    EF, EI, Q, Qit, C)) {
@@ -81,6 +97,13 @@ int main(int argc, char *argv[]) {
                 }
                 something_collapsed = true;
                 num_collapsed++;
+            }
+            for (int i = 0; i < F.rows(); i++) {
+                if (F(i, 0) == 0 && F(i, 1) == 0 && F(i, 2) == 0) {
+                    mods.push_back(
+                        MeshModification(OOV.row(OF(i, 0)), OOV.row(OOF(i, 1)),
+                                         OOV.row(OF(i, 2)), i, OOF.row(i)));
+                }
             }
             // ZEROD: F,E,EF,EI
             if (something_collapsed) {
@@ -90,6 +113,21 @@ int main(int argc, char *argv[]) {
             }
         }
         return false;
+    };
+
+    const auto &uncollapse_edges = [&](igl::viewer::Viewer &viewer) -> bool {
+        if (viewer.core.is_animating && !mods.empty()) {
+            MeshModification mod = mods.back();
+            mods.pop_back();
+            cout << "mod fi" << mod.fi << endl;
+            F.row(mod.fi) = mod.f;
+            V.row(F(mod.fi, 0)) = mod.v1;
+            V.row(F(mod.fi, 1)) = mod.v2;
+            V.row(F(mod.fi, 2)) = mod.v3;
+        }
+        viewer.data.clear();
+        viewer.data.set_mesh(V, F);
+        viewer.data.set_face_based(true);
     };
 
     const auto &key_down = [&](igl::viewer::Viewer &viewer, unsigned char key,
@@ -102,6 +140,12 @@ int main(int argc, char *argv[]) {
         case 'r':
             reset();
             break;
+        case '1':
+            collapse_edges(viewer);
+            break;
+        case '2':
+            uncollapse_edges(viewer);
+            break;
         default:
             return false;
         }
@@ -111,6 +155,6 @@ int main(int argc, char *argv[]) {
     reset();
     viewer.core.is_animating = true;
     viewer.callback_key_down = key_down;
-    viewer.callback_pre_draw = pre_draw;
+    // viewer.callback_pre_draw = pre_draw;
     return viewer.launch();
 }
