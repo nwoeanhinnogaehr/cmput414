fcc3f2a3d8c25e34d2ea9c323b003fe482248e0f
Noah Weninger <nweninge@ualberta.ca>
2016-03-16 21:12:49 -0600

    improve message formatting

--- test/collapse_edge.cpp
+++ test/collapse_edge.cpp
@@ -116,7 +116,8 @@ IGL_INLINE bool collapse_edge(const int e, const Eigen::RowVectorXd &p,
                        E(EMAP(f + m * ((v + 2) % 3)), flip2) == s);
                 E(EMAP(f + m * ((v + 2) % 3)), flip2) = s;
                 F(f, v) = s;
-                std::cout << "C" << f << ": " << v << "=" << s << std::endl;
+                std::cout << "Set F(" << f << "," << v << ") = " << s
+                          << std::endl;
                 a_f3 = f;
                 break;
             }
