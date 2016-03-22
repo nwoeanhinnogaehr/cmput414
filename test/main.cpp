50ef61bb335e1d7a58ac5eab16392ed6d4ccf72a
Noah Weninger <nweninge@ualberta.ca>
2016-03-22 17:16:07 -0600

    indent

--- test/main.cpp
+++ test/main.cpp
@@ -13,17 +13,16 @@ using namespace Eigen;
 using namespace igl;
 
 struct MeshModification {
-  int vi1;
-  int vi2;
-  RowVectorXd v1;
-  RowVectorXd v2;
-  std::vector<int> faceInd;
-  MatrixXi faces;
-  MeshModification(int vi1, int vi2, RowVectorXd v1, RowVectorXd v2,
-		   std::vector<int> faceInd, MatrixXi faces)
-    : vi1(vi1), vi2(vi2), v1(v1), v2(v2),faceInd(faceInd), faces(faces) {}
+    int vi1;
+    int vi2;
+    RowVectorXd v1;
+    RowVectorXd v2;
+    std::vector<int> faceInd;
+    MatrixXi faces;
+    MeshModification(int vi1, int vi2, RowVectorXd v1, RowVectorXd v2,
+                     std::vector<int> faceInd, MatrixXi faces)
+        : vi1(vi1), vi2(vi2), v1(v1), v2(v2), faceInd(faceInd), faces(faces) {}
 };
-
 
 int main(int argc, char *argv[]) {
     cout << "Usage: " << argv[0] << " [filename.(off|obj|ply)]" << endl;
@@ -61,7 +60,7 @@ int main(int argc, char *argv[]) {
         const Eigen::MatrixXi & /*EF*/, const Eigen::MatrixXi & /*EI*/,
         double &cost, RowVectorXd &p) {
         // manhattan
-        //cost = (V.row(E(e, 0)) - V.row(E(e, 1))).cwiseAbs().sum();
+        // cost = (V.row(E(e, 0)) - V.row(E(e, 1))).cwiseAbs().sum();
         // euclidean
         cost = (V.row(E(e, 0)) - V.row(E(e, 1))).norm();
         p = 0.5 * (V.row(E(e, 0)) + V.row(E(e, 1)));
@@ -95,7 +94,8 @@ int main(int argc, char *argv[]) {
         if (viewer.core.is_animating && !Q.empty()) {
             bool something_collapsed = false;
             // collapse edge
-            const int max_iter = std::ceil(0.1 * Q.size());;
+            const int max_iter = std::ceil(0.1 * Q.size());
+            ;
             MatrixXd OOV = V;
             MatrixXi OOF = F;
             MatrixXi OOE = E;
@@ -103,24 +103,23 @@ int main(int argc, char *argv[]) {
             MatrixXi OEI = EI;
             VectorXi OEMAP = EMAP;
             for (int j = 0; j < max_iter; j++) {
-	      int e, e1, e2, f1, f2, f3;
-	      std::vector<int> faceInd;
-	      
+                int e, e1, e2, f1, f2, f3;
+                std::vector<int> faceInd;
+
                 if (!collapse_edge(shortest_edge_and_midpoint, V, F, E, EMAP,
-                                   EF, EI, Q, Qit, C, e, e1, e2, f1, f2, faceInd)) {
+                                   EF, EI, Q, Qit, C, e, e1, e2, f1, f2,
+                                   faceInd)) {
                     break;
                 }
-		
-		MatrixXi faces(faceInd.size() + 2, 3);
-		faceInd.push_back(f1);
-		faceInd.push_back(f2);
-		for(int i = 0; i < faceInd.size(); i++) {
-		  faces.row(i) = OOF.row(faceInd[i]);
-		  cout << "ffF" << faces.row(i) << endl;
-		}
 
+                MatrixXi faces(faceInd.size() + 2, 3);
+                faceInd.push_back(f1);
+                faceInd.push_back(f2);
+                for (int i = 0; i < faceInd.size(); i++) {
+                    faces.row(i) = OOF.row(faceInd[i]);
+                    cout << "ffF" << faces.row(i) << endl;
+                }
 
-		
                 mods.push_back(
                     MeshModification(OOE(e, 0), OOE(e, 1), OOV.row(OOE(e, 0)),
                                      OOV.row(OOE(e, 1)), faceInd, faces));
@@ -137,27 +136,20 @@ int main(int argc, char *argv[]) {
     };
 
     const auto &uncollapse_edges = [&](igl::viewer::Viewer &viewer) -> bool {
-        if (viewer.core.is_animating && !mods.empty()) {            
-            
-            
+        if (viewer.core.is_animating && !mods.empty()) {
+
             MeshModification mod = mods.back();
             mods.pop_back();
             V.row(mod.vi1) = mod.v1;
             V.row(mod.vi2) = mod.v2;
 
-	    for(int i = 0; i < mod.faceInd.size(); i++) {
-	      F.row(mod.faceInd[i]) = mod.faces.row(i);
-	    }
-            
-         
-    
+            for (int i = 0; i < mod.faceInd.size(); i++) {
+                F.row(mod.faceInd[i]) = mod.faces.row(i);
+            }
+
             viewer.data.clear();
             viewer.data.set_mesh(V, F);
             viewer.data.set_face_based(true);
-
-
-
-                
         }
     };
 
